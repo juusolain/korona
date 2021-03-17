ea06d4e2f66fa8e12f603dfafd44f1dc400b85a1
jusola <juulain@gmail.com>
2021-03-17 17:49:14 +0200

    Remove unused stuff from main

--- vaccines.js
+++
@@ -1,4 +0,0 @@
-// const url = "https://sampo.thl.fi/pivot/prod/fi/vaccreg/cov19cov/fact_cov19cov.json?filter=measure-533175&column=dateweek20201226-525425&row=cov_vac_age-518413&row=area-518362"
-JSONstat("./cached-response.json").then(J=> {
-    console.log(J.Dataset( 0 ).toTable({ type: 'arrobj' }))
-})
